36e084da8fbaedb0e98bbfb13475041d543ffdde
Stuart Williams <stuart.williams@dice.fm>
2022-10-14 09:19:05 +0100

    dasad

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ChordSVG from "react-chord-svg";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(
@@ -6,6 +7,53 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <ChordSVG
+      width="500"
+      title="C Major - E Shape"
+      numberOfFrets={4}
+      startAtFret={8}
+      notes={[
+        {
+          fret: 1,
+          string: 1,
+          bar: 6,
+          color: "gray",
+          opacity: 0.5,
+        },
+        {
+          fret: 1,
+          string: 1,
+          text: "1",
+          color: "red",
+        },
+        {
+          fret: 3,
+          string: 2,
+          text: "3",
+        },
+        {
+          fret: 3,
+          string: 3,
+          text: "4",
+          color: "red",
+        },
+        {
+          fret: 2,
+          string: 4,
+          text: "2",
+        },
+        {
+          fret: 1,
+          string: 5,
+          text: "1",
+        },
+        {
+          fret: 1,
+          string: 6,
+          text: "1",
+          color: "red",
+        },
+      ]}
+    />
   </React.StrictMode>
 );

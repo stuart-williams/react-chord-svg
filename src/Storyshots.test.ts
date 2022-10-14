9b7e3228482925149c374d8f3957513e5dbba967
Stuart Williams <stuart.williams@dice.fm>
2022-10-14 08:22:52 +0100

    reduce storyshot threshold

--- src/Storyshots.test.ts
+++ src/Storyshots.test.ts
@@ -1,4 +1,12 @@
 import initStoryshots from "@storybook/addon-storyshots";
 import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer";
 
-initStoryshots({ suite: "Image storyshots", test: imageSnapshot() });
+initStoryshots({
+  suite: "Image storyshots",
+  test: imageSnapshot({
+    getMatchOptions: () => ({
+      failureThreshold: 0.2,
+      failureThresholdType: "percent",
+    }),
+  }),
+});

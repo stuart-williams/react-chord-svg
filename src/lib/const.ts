3bc509438a3797991a02c52a2a6894d1ceb8a056
stuart-williams <stuwilliams47@gmail.com>
2022-10-15 07:28:08 +0000

    Deploying to gh-pages from @ stuart-williams/react-chord-svg@7f64b9f9792d91062ae213dd9fd593167f3b5066 🚀

--- src/lib/const.ts
+++
@@ -1,6 +0,0 @@
-export const noteRadius = 24;
-export const openNoteRadius = noteRadius * 0.8;
-export const mutedNoteSize = noteRadius * 0.7;
-export const fretboardWidth = 300;
-export const titleFontSize = 30;
-export const fontFamily = "Arial, Helvetica, sans-serif";
--- src/lib/utils.ts
+++
@@ -1,4 +0,0 @@
-export const times = <T = number>(n: number, cb: (i: number) => any): T[] =>
-  Array(n)
-    .fill(0)
-    .map((_, i) => cb(i));

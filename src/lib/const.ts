49887794b92c922428072e6cf1e122f00a7dbae8
Stuart Williams <stuart.williams@dice.fm>
2022-10-18 21:14:37 +0100

    open notes size

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,7 +1,7 @@
 export const padding = 20;
-export const noteRadius = 24;
-export const openNoteRadius = noteRadius * 0.8;
-export const mutedNoteSize = noteRadius * 0.7;
+export const noteRadius = 20;
+export const openNoteRadius = noteRadius - 1;
+export const mutedNoteSize = noteRadius - 1;
 export const fretboardWidth = 300;
 export const titleFontSize = 30;
 export const fontFamily = "Arial, Helvetica, sans-serif";

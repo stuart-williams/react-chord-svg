683f25aae8dfb415fd97fcb5f741dacbd382eac5
Stuart Williams <stuart.williams@dice.fm>
2022-10-28 16:08:00 +0100

    fix text alignment bug

--- src/lib/Title.tsx
+++ src/lib/Title.tsx
@@ -1,16 +1,16 @@
 import { FC, SVGProps } from "react";
 import { fontFamily, titleFontSize } from "./const";
 
-const Title: FC<SVGProps<SVGTextElement>> = ({
-  children,
-  fontSize = titleFontSize,
-  ...props
-}) => (
+interface Props extends SVGProps<SVGTextElement> {
+  fontSize?: number;
+}
+
+const Title: FC<Props> = ({ children, fontSize = titleFontSize, ...props }) => (
   <text
+    dy={fontSize / 2}
     textAnchor="middle"
     fontSize={fontSize}
     fontFamily={fontFamily}
-    alignmentBaseline="central"
     {...props}
   >
     {children}

a5b61453ec085fd610b93596667fa7a8fa49c81e
Stuart Williams <stuart.williams@dice.fm>
2022-10-31 12:34:57 +0000

    muted note size

--- src/lib/MutedNote.tsx
+++ src/lib/MutedNote.tsx
@@ -5,31 +5,27 @@ export interface MutedNoteProps {
   x: number;
 }
 
-const MutedNote: FC<MutedNoteProps> = ({ x }) => {
-  const radius = noteRadius * 0.9;
-
-  return (
-    <svg x={x - radius} y={-(radius * 2.5 + 1)} overflow="visible">
-      <line
-        x1="0"
-        y1="0"
-        stroke="black"
-        y2={radius * 2}
-        x2={radius * 2}
-        strokeWidth="2"
-        strokeLinecap="round"
-      />
-      <line
-        y1="0"
-        x2="0"
-        stroke="black"
-        y2={radius * 2}
-        x1={radius * 2}
-        strokeWidth="2"
-        strokeLinecap="round"
-      />
-    </svg>
-  );
-};
+const MutedNote: FC<MutedNoteProps> = ({ x }) => (
+  <svg x={x - noteRadius} y={-(noteRadius * 2.5 + 1)} overflow="visible">
+    <line
+      x1="0"
+      y1="0"
+      stroke="black"
+      y2={noteRadius * 2}
+      x2={noteRadius * 2}
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+    <line
+      y1="0"
+      x2="0"
+      stroke="black"
+      y2={noteRadius * 2}
+      x1={noteRadius * 2}
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+  </svg>
+);
 
 export default MutedNote;
